05b11491ab94113a880f9bcacb51aa36d6a380ae
Stefan Moog <moogs@gmx.de>
2026-03-08 08:13:22 +0100

    Style

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Fade-in content only (chart removed)
 window.addEventListener("load", () => {
-    const el = document.querySelector(".fade-in");
-    if (el) el.classList.add("visible");
+  const el = document.querySelector(".fade-in");
+  if (el) el.classList.add("visible");
 });
